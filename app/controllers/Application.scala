77f94f24486a67341d135d2f02b9eb29942aaea1
PiotrGoralczyk <pgoralcz@student.agh.edu.pl>
2014-03-31 20:42:48 +0200

    Add exclamation mark

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready. So let's start coding, John Doe"))
+    Ok(views.html.index("Your new application is ready. So let's start coding, John Doe!"))
   }
 
 }
