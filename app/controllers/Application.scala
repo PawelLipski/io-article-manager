6213d641713547e44c166a534272351053ea8d44
PiotrGoralczyk <pgoralcz@student.agh.edu.pl>
2014-04-01 00:53:53 +0200

    Create PDF generation demo

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import utils.PdfGenerator
+import java.util.Date
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready. So let's start coding, John Doe!"))
+  }
+
+  def pdfGenerationDemo = Action {
+    Ok(PdfGenerator.generate("Hello PDF!\n" + new Date().toString)).as("application/pdf")
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -4,4 +4,6 @@ name := "IOArticleManager"
 
 version := "1.0"
 
+libraryDependencies += "com.itextpdf" % "itextpdf" % "5.1.1"
+
 playScalaSettings
