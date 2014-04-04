4ebaa9ac664101b5ff3f412b844b3a52f147b2a1
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-04-04 23:03:07 +0200

    Connect with JDBC to the Postgres provided by Heroku

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,16 +3,13 @@ package controllers
 import play.api._
 import play.api.mvc._
 import java.sql.{Connection, DriverManager, ResultSet}
-import com.mysql.jdbc.Driver
 
 object Application extends Controller {
 
-  def index = Action {
-
-    var contents = "Your new application is ready. So let's start coding, John Doe! MySQL: ";
-
+  private def mysql_test = {
     val passwd = System.getenv("OJS_DB_PASSWD")
     val conn_str = "jdbc:mysql://sql.udl.pl:3306/slonka_ojs238?user=slonka_ojs&password=" + passwd
+    var contents = ""
 
     classOf[com.mysql.jdbc.Driver]
 
@@ -29,9 +26,48 @@ object Application extends Controller {
     } finally {
       conn.close()
     }
+    contents
+  }
+
+  def pgsql_test = {
+
+    val passwd = System.getenv("INTERNAL_DB_PASSWD")
+    val user = "zlamwfnyrreuew"
+    val conn_str = "jdbc:postgresql://ec2-54-246-101-204.eu-west-1.compute.amazonaws.com:5432/d9ml8v51vhnu5u?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
+    var contents = ""
+
+    println(classOf[org.postgresql.Driver])
+
+    val conn = DriverManager.getConnection(conn_str, user, passwd)
+    try {
+
+      val statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+
+      val rs = statement.executeQuery("SELECT * FROM authors")
+
+      while (rs.next) {
+        contents += rs.getString("email") + " "
+      }
+    } finally {
+      conn.close()
+    }
+    contents
+  }
+
+  def index = Action {
+
+    var contents = "Your new application is ready. So let's start coding, John Doe! "
+
+    contents += "MySQL: "
+    contents += mysql_test
+
+    contents += "PgSQL: "
+    contents += pgsql_test
 
     Ok(views.html.index(contents))
   }
+
+
 
 }
 
--- build.sbt
+++ build.sbt
@@ -9,5 +9,6 @@ playScalaSettings
 libraryDependencies ++= Seq(
   jdbc,
   cache,
-  "mysql" % "mysql-connector-java" % "5.1.30"
+  "mysql" % "mysql-connector-java" % "5.1.30",
+  "org.postgresql" % "postgresql" % "9.3-1100-jdbc4"
 )
