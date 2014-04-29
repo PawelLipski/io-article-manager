59f57e4de01f526304b69f7ca9f89e9a579bc067
matips <mateusz.pszczolka@gmail.com>
2014-04-29 23:15:11 +0200

    links in menu and list of authors/rewiewers

---
+++ app/models/reports/AuthorList.scala
@@ -0,0 +1,12 @@
+package models.reports
+
+import models.reports.ArticleStatus.ArticleStatus
+
+/**
+ * Author: Mateusz Pszczółka <mateusz.pszczolka@gmail.com>
+ * Date: 4/18/2014
+ * Time: 1:44 PM
+ */
+case class AuthorList (journal: Journal, year : Int, articleStatus: ArticleStatus){
+
+}
