6aec21e30c26ce2ae1bbd398c93cfa80eb5658d5
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-05-14 17:03:07 +0200

    Tidy up the project

--- app/controllers/Authentication.scala
+++ app/controllers/Authentication.scala
@@ -7,28 +7,7 @@ import play.api.data.Forms._
 import models.dao.AuthenticationDao
 import org.apache.commons.codec.binary.Hex
 
-trait Secured {
-  self: Controller =>
-
-  /**
-   * Retrieve the connected user id.
-   */
-  def username(request: RequestHeader) = request.session.get("user")
-
-  /**
-   * Redirect to login if the use in not authorized.
-   */
-  def onUnauthorized(request: RequestHeader): SimpleResult =
-    Results.Redirect(routes.AuthenticationController.login()).withSession(("returnUrl", request.path))
-
-
-  def withAuth(f: => String => Request[AnyContent] => Result) =
-    Security.Authenticated(username, onUnauthorized) { user =>
-      Action(request => f(user)(request))
-    }
-}
-
-object AuthenticationController extends Controller with Secured {
+object Authentication extends Controller with Secured {
 
   lazy val loginForm = Form(
     tuple(
@@ -41,28 +20,27 @@ object AuthenticationController extends Controller with Secured {
   )
 
   private def verifyCredentials(user: String, password: String): Boolean = {
+
     val sha1 = java.security.MessageDigest.getInstance("SHA-1")
     val actualSha1Sum = new String(Hex.encodeHex(sha1.digest(password.getBytes)))
-    println(actualSha1Sum)
     val expectedSha1Sum = AuthenticationDao.getPasswordSha1SumForUser(user)
-    println(expectedSha1Sum)
     actualSha1Sum == expectedSha1Sum
   }
 
   def doLogin = Action { implicit request =>
     Logger.info("Authenticating user")
     loginForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(views.html.login(formWithErrors, routes.AuthenticationController.doLogin())),
+      formWithErrors => BadRequest(views.html.login(formWithErrors, routes.Authentication.doLogin())),
       user => Redirect(session.get("returnUrl").getOrElse("/")).withSession(session + ("user" -> user._1) - "returnUrl")
     )
   }
 
   def login = Action { implicit request =>
-    Ok(views.html.login(loginForm, routes.AuthenticationController.doLogin()))
+    Ok(views.html.login(loginForm, routes.Authentication.doLogin()))
   }
 
   def logout = Action {
-    Redirect(routes.AuthenticationController.login).withNewSession.flashing(
+    Redirect(routes.Authentication.login).withNewSession.flashing(
       "success" -> "You've been logged out"
     )
   }
---
+++ app/controllers/Secured.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import play.api.mvc._
+import play.api.mvc.SimpleResult
+
+trait Secured {
+  self: Controller =>
+
+  /**
+   * Retrieve the connected user id.
+   */
+  def username(request: RequestHeader) = request.session.get("user")
+
+  /**
+   * Redirect to login if the use in not authorized.
+   */
+  def onUnauthorized(request: RequestHeader): SimpleResult =
+    Results.Redirect(routes.Authentication.login()).withSession(("returnUrl", request.path))
+
+
+  def withAuth(f: => String => Request[AnyContent] => Result) =
+    Security.Authenticated(username, onUnauthorized) { user =>
+      Action(request => f(user)(request))
+    }
+}
