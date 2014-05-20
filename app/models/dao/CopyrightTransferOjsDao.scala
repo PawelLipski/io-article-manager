d2f1b71a38ba653dbf97f8e31d36d5407ce714a1
slonka <slonka@student.agh.edu.pl>
2014-05-20 10:40:03 +0200

    Mateusz temp zmiany

--- app/models/dao/CopyrightTransferOjsDao.scala
+++ app/models/dao/CopyrightTransferOjsDao.scala
@@ -14,39 +14,26 @@ import java.sql.Date
 import scala.slick.lifted
 import slick.ojs.Tables
 import slick.ojs
+import org.h2.engine.Database
 
 /**
  * Created by slonka on 20.05.14.
  */
 object CopyrightTransferOjsDao {
 
-  /* select * from slonka_ojs238.authors as ojs_a join slonka_io.CopyrightTransfer as io_c left join
-   slonka_io.AuthorsContribution io_a on ojs_a.submission_id = io_c.ojsArticleId and io_c.id = io_a.id
-   where ojs_a.submission_id = 1 */
 
-  /*
   def getAuthorsForArticle(ojsArticleId: Int): CopyrightTransferRequest = {
     var authors :Seq[AuthorsRow] = Seq(0, 1)
 
     Database.forDataSource(DB.getDataSource("ojs")).withSession {
       implicit session =>
-        authors = slick.ojs.Tables.Authors.filter(_.submissionId === ojsArticleId.asLong)
 
-        var title =
-          slick.ojs.Tables.ArticleSettings.filter(_.articleId === ojsArticleId.asLong, _.settingName === "title")
+        (for{
+          author <- slick.ojs.Tables.Authors if author.submissionId === ojsArticleId.asLong
+          title <- slick.ojs.Tables.ArticleSettings if title.settingName === "title" && title.articleId === ojsArticleId.asLong
+          affiliation <- slick.ojs.Tables.ArticleSettings if affiliation.settingName === "affiliation" && affiliation.articleId === ojsArticleId.asLong
 
-        var affiliation =
-          slick.ojs.Tables.ArticleSettings.filter(_.articleId === ojsArticleId.asLong, _.settingName === "affiliation")
+        } yield (author, title, affiliation)).list.map()
     }
-
-    var correspondingAuthor = authors.filter(_.primaryContact === 1)
-
-
-    TODO SOMEONE ELSE WILL NOT COMLETE ON TIME
-    CopyrightTransferRequest(ojsArticleId,
-      Copyright(ojsArticleId, title,
-       CorrespondingAuthor( correspondingAuthor.lift(0).firstName,  )
-      )
-    )
-  }*/
+  }
 }
