4b60c941804513ca2d1d6521ea8f37026a95963b
slonka <slonka@student.agh.edu.pl>
2014-05-21 02:39:31 +0200

    Binded year to view, others need dao and I need sleep

--- app/controllers/Authors.scala
+++ app/controllers/Authors.scala
@@ -10,26 +10,11 @@ import models.dao.CopyrightTransferInternalDao
 
 object Authors extends Controller with Secured {
 
-  val authors: scala.collection.mutable.Map[Int, Author] = new HashMap
-
-  val form = Form(
-    mapping(
-      "papertitle" -> text,
-      "username" -> text,
-      "lastname" -> text,
-      "affiliation" -> text,
-      "email" -> text,
-      "date_filled" -> text,
-      "place" -> text)
-      (Author.apply)(Author.unapply)
-  )
-
   def list(id: Int, year: Int, volume_id: Int) = withAuth {
     var authorsSlick = CopyrightTransferInternalDao.listTransfer(id, year, volume_id)
     var journals = RankingDataExtractorOjsDao.getListOfJournals
-    var parameters = (id, year, volume_id);
 
     user => implicit request =>
-      Ok(views.html.authors.list(parameters, authorsSlick, journals))
+      Ok(views.html.authors.list(id, year, volume_id, authorsSlick, journals))
   }
 }
