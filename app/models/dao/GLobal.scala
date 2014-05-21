91219e22190d572936fdb3fe368d8c93452a71b5
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-05-21 09:09:50 +0200

    Finish up 404/error/handler not found pages

--- app/models/dao/GLobal.scala
+++ app/models/dao/GLobal.scala
@@ -2,24 +2,23 @@ import play.api.GlobalSettings
 import play.api.mvc.RequestHeader
 import play.api.mvc.Results._
 import scala.concurrent._
-import scala.slick.driver.{JdbcProfile, MySQLDriver}
 import ExecutionContext.Implicits.global
 
 object Global extends GlobalSettings {
 
   // called when a route is found, but it was not possible to bind the request parameters
   override def onBadRequest(request: RequestHeader, error: String) = future {
-    BadRequest("Bad Request: " + error)
+    BadRequest(views.html.errors.onHandlerNotFound()(request))
   }
 
   // 500 - internal server error
   override def onError(request: RequestHeader, throwable: Throwable) = future {
-    InternalServerError(views.html.errors.onError(throwable))
+    InternalServerError(views.html.errors.onError(throwable)(request))
   }
 
   // 404 - page not found error
   override def onHandlerNotFound(request: RequestHeader) = future {
-    NotFound(views.html.errors.onHandlerNotFound(request))
+    NotFound(views.html.errors.onHandlerNotFound()(request))
   }
 
 }
