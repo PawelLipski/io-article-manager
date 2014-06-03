3897ddced41db75dd206b8a222cea070af534667
matips <mateusz.pszczolka@gmail.com>
2014-06-03 13:25:41 +0200

    fix to Kuba

--- app/models/dao/GeneralOjsDao.scala
+++ app/models/dao/GeneralOjsDao.scala
@@ -6,44 +6,38 @@ package models.dao
 
 import play.api._
 import play.api.mvc._
-import java.sql.{DriverManager, ResultSet}
-import models.copyright.{Contribution, CorrespondingAuthor, CopyrightTransferRequest, Copyright}
+import java.sql.ResultSet
+import models.copyright.Contribution
 import scala.slick.driver.MySQLDriver.simple._
-import scala.slick.driver.MySQLDriver
 import play.api.db.DB
-import utils.{SqlUtils, TokenGenerator}
+import utils.TokenGenerator
 import play.api.Play.current
-import com.google.common.base.Optional
 import java.sql.Date
-import scala.slick.lifted
 import slick.ojs.Tables
 import slick.ojs
 
 object GeneralOjsDao {
 
-    val yearFn = SimpleFunction[Int]("year")
+  val yearFn = SimpleFunction[Int]("year")
 
-    def getYearsJournalActive(journalId: Int) :List[Int] = {
-      Database.forDataSource(DB.getDataSource("ojs")).withSession {
-        implicit session => {
-          val years = slick.ojs.Tables.Articles.filter(_.journalId === journalId.asInstanceOf[Long]).map(x => yearFn(Seq(x.dateSubmitted)))
-          //years.list.distinct
-          years.list
-        }
-      }
-    }
-
-    def getIssuesForJournal(journalId: Int) : List[(Int,String )] = {
-      Database.forDataSource(DB.getDataSource("ojs")).withSession {
+  def getYearsJournalActive(journalId: Int): List[Int] = {
+    Database.forDataSource(DB.getDataSource("ojs")).withSession {
       implicit session => {
-        for {
-          article <- slick.ojs.Tables.Articles if article.journalId === journalId.asInstanceOf[Long]
-          articleSetting <- slick.ojs.Tables.ArticleSettings if articleSetting.settingName === "title" && article.articleId === articleSetting.articleId
-        } yield()
-
-        val issueList = slick.ojs.Tables.Issues.filter(_.journalId === journalId.asInstanceOf[Long]).map(f=>(f.issueId, f.showTitle)).list
-      issueList
+        val years = slick.ojs.Tables.Articles.filter(_.journalId === journalId.asInstanceOf[Long]).map(x => yearFn(Seq(x.dateSubmitted)))
+        //years.list.distinct
+        years.list
       }
     }
+  }
+
+  def getIssuesForJournal(journalId: Int): List[(Long, String)] = {
+    Database.forDataSource(DB.getDataSource("ojs")).withSession {
+      implicit session => {
+        (for {
+          issue <- slick.ojs.Tables.Issues if issue.journalId === journalId.asInstanceOf[Long]
+          issueSetting <- slick.ojs.Tables.IssueSettings if issueSetting.settingName === "title" && issue.issueId === issueSetting.issueId
+        } yield issueSetting).list.filter(_.settingValue.isDefined).map(x => (x.issueId, x.settingValue.get))
+      }
     }
+  }
 }
