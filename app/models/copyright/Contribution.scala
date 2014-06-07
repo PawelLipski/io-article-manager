e7a8ee14c646fd98c3de4de2e7e160910a905c58
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-06-07 14:23:43 +0200

    Implement Contributions table class

--- app/models/copyright/Contribution.scala
+++ app/models/copyright/Contribution.scala
@@ -1,8 +1,7 @@
 package models.copyright
 
-/**
- * Created by Zeuko on 07.04.14.
- */
+import scala.slick.driver.MySQLDriver.simple._
+
 case class Contribution(firstName : String,
                         middleName: Option[String],
                         lastName: String,
@@ -15,8 +14,32 @@ case class Contribution(firstName : String,
       contribution+"\n\rContribution percentage: "+
       percent+"%"
 
-  def getFullAuthorName : String = {
-    firstName + " " + middleName.getOrElse("") + " " + lastName
-  }
+  def getFullAuthorName = firstName + " " + middleName.getOrElse("") + " " + lastName
+}
 
+class Contributions(tag: Tag)
+  extends Table[Contribution](tag, Contributions.TABLE_NAME) {
+
+  // TODO: add to the case class
+  //def id = column[Int]("id", O.PrimaryKey)
+
+  def firstName = column[String]("firstName")
+
+  def middleName = column[Option[String]]("middleName")
+
+  def lastName = column[String]("lastName")
+
+  def affiliation = column[String]("affiliation")
+
+  def contribution = column[String]("contribution")
+
+  def percent = column[Int]("percent")
+
+  def * = (firstName, middleName, lastName, affiliation, contribution, percent) <>
+    (Contribution.tupled, Contribution.unapply)
+}
+
+object Contributions {
+  val TABLE_NAME = "CONTRIBUTIONS"
+  val contributions = TableQuery[Contributions]
 }
