cd83d3b60607d65703a39c58adec6b4aa41d131d
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-06-07 11:38:20 +0200

    Move USERS table auto-creation to DatabaseSessionWrapper

--- app/models/dao/AuthenticationDao.scala
+++ app/models/dao/AuthenticationDao.scala
@@ -7,23 +7,9 @@ import utils.DatabaseSessionWrapper._
 
 object AuthenticationDao {
 
-  /*def printUserNames {
-    Database.forDataSource(DB.getDataSource("internal")).withSession {
-      implicit session =>
-        users.ddl.create
-        users += (1, "admin", "4a0c5a8df03ea9c60cea9a4b876f97f716e42dbf") // ala123
-        users foreach {
-          case (id, name, passwordSha1Sum) =>
-            println(id + "  " + name + "  " + passwordSha1Sum)
-        }
-    }
-  }*/
-
   def getPasswordSha1SumForUser(userName: String): Option[String] = {
     withInternalDatabaseSession {
       implicit session =>
-        if (MTable.getTables("USERS").list.isEmpty)
-          users.ddl.create
         val found = users.filter(_.name === userName).map(_.passwordSha1Sum)
         found.firstOption
     }
--- app/utils/DatabaseSessionWrapper.scala
+++ app/utils/DatabaseSessionWrapper.scala
@@ -3,15 +3,27 @@ package utils
 import play.api.db.DB
 import scala.slick.driver.MySQLDriver.simple._
 import play.api.Play.current
+import scala.slick.jdbc.meta.MTable
+import models.authentication.Users._
 
 object DatabaseSessionWrapper {
 
-  def withSession[T](dbName: String, f: Session => T): T = {
-    Database.forDataSource(DB.getDataSource(dbName)).withSession(f)
+  def withSession[T](dbName: String)(block: Session => T): T = {
+    Database.forDataSource(DB.getDataSource(dbName)).withSession(block)
   }
 
-  def withInternalDatabaseSession[T](f: Session => T): T = withSession("internal", f)
+  def ensureInternalDatabaseTablesExist(implicit session: Session) {
+    if (MTable.getTables("USERS").list.isEmpty)
+      users.ddl.create
+  }
 
-  def withOjsDatabaseSession[T](f: Session => T): T = withSession("ojs", f)
+  def withInternalDatabaseSession[T](block: Session => T): T = withSession("internal") {
+    session =>
+      ensureInternalDatabaseTablesExist(session)
+      block(session)
+  }
 
+  def withOjsDatabaseSession[T](block: Session => T): T = withSession("ojs") {
+    block
+  }
 }
