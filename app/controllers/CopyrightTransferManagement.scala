ec0f2281f6276cc1acc03c2ed59132b0a4c93968
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-06-09 16:31:34 +0200

    Rename routes and the Authors controller

--- app/controllers/CopyrightTransferManagement.scala
+++ app/controllers/CopyrightTransferManagement.scala
@@ -1,14 +1,10 @@
 package controllers
 
 import play.api.mvc._
-import play.api.data._
-import play.api.data.Forms._
-import play.api.data.validation._
-import models.{RankingDataExtractorOjsDao, Author}
-import scala.collection.mutable.HashMap
+import models.RankingDataExtractorOjsDao
 import models.dao.CopyrightTransferInternalDao
 
-object Authors extends Controller with Secured {
+object CopyrightTransferManagement extends Controller with Secured {
 
   def list(id: Int, year: Int, volumeId: Int) = withAuth {
     val transferRequests = CopyrightTransferInternalDao.listTransferRequests(id, year, volumeId)
