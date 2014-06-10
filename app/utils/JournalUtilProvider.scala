0d1e9420e31d65bebfaa4f4ce14f6b9430f7b100
plisowska <lisowska@student.agh.edu.pl>
2014-06-10 11:29:57 +0200

    Added error message

--- app/utils/JournalUtilProvider.scala
+++ app/utils/JournalUtilProvider.scala
@@ -4,6 +4,8 @@ import com.itextpdf.text.Image
 import play.api.Play
 import play.api.Play.current
 import scala.io._
+import java.nio.file.{Paths, Path, Files}
+import java.io.{File, FileNotFoundException}
 
 
 /**
@@ -12,17 +14,29 @@ import scala.io._
 object JournalUtilProvider {
 
   def getLogoAssetsPath(journalID: Long): String = {
-    "/assets/resources/" + journalID + "/logo.png"
+    val path = "/assets/resources/" + journalID + "/logo.png"
+    if (!new File(path).exists()) {
+      throw new FileNotFoundException("Logo for journal id "+journalID+" is missing.");
+    }
+    path
   }
 
   def getLogoImage(journalID: Long): Image = {
     val path = "public/resources/" + journalID + "/logo.png"
-    Image.getInstance(Play.resource(path).get)
+    if (new File(path).exists()) {
+      Image.getInstance(Play.resource(path).get)
+    } else {
+      throw new FileNotFoundException("Logo for journal id "+journalID+" is missing.");
+    }
   }
 
   def getConsentToPublishText(journalID: Long): List[String] = {
     val path = "public/resources/" + journalID + "/ctp.txt"
-    Source.fromInputStream(Play.resourceAsStream(path).get).getLines().toList
+    if (new File(path).exists()) {
+      Source.fromInputStream(Play.resourceAsStream(path).get).getLines().toList
+    } else {
+      throw new FileNotFoundException("Consent text for journal id "+journalID+" is missing.");
+    }
   }
 
 }
